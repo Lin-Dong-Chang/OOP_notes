cdcbe772be66939483ffa2b2475bc2b996400fbc
DongChangLin <dong@lindongzhangdeMacBook-Pro.local>
2021-07-22 16:09:45 +0800

    0722_update

---
+++ notes/geometry.py
@@ -0,0 +1,5 @@
+class PPoint:                         # need an X and Y coordinate
+                                     # NEEDS to create __init__
+    def __init__(self, x, y):        # holds the minimum parameters of the creating objects
+        self.x = x
+        self.y = y
